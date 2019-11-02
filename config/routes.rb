ff926dada7f55886835b00bcd33ded0f2039cc6c
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-02 14:13:24 +0100

    configured admin, created devise user and added devise user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+  	registrations: 'registrations'
+  }
+  
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root "static_pages#index"
 end
