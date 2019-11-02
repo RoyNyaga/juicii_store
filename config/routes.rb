895f3c2663a9b9d0d95504a84acf88c93c34fe46
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-03 00:12:20 +0100

    styled session forms, registration forms, created navbar and header partials

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root "static_pages#index"
+  root "products#index"
+  resources :products,       only: [:index, :show]
 end
