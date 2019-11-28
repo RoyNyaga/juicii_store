a1fc0c0468a5d6467d9f7c8fff17a4832ec517c0
Roy Nyaga <nyagaandreroy@gmail.com>
2019-11-28 22:33:34 +0100

    completed search form

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,13 +1,16 @@
 class SearchResultsController < ApplicationController
 
 	def index
-		if params[:search].blank?
-			render "search_results/index"
+		
+
+		if params[:search][:search].empty?
+			flash[:info] = "Nothing To Search"
+			redirect_back(fallback_location: root_path)
 		else
-			@parameter = params[:search].downcase
+			@parameter = params[:search][:search].downcase
 			@product_search = Product.search(@parameter)
 			@category_search = Category.search(@parameter)
-			@search_result = @product_search.count + @category_search.count if params[:search]
+			@search_result = @product_search.count + @category_search.count if params[:search][:search]
 		end		
 	end
 end
