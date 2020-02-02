ec9fe390fdafc58ea2b8e8d5966865b263baf50f
Roy Nyaga <nyagaandreroy@gmail.com>
2020-02-02 12:38:04 +0100

    implemented facebook login at the development level

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,27 @@ class User < ApplicationRecord
          :omniauthable, omniauth_providers: %i[facebook]
   has_many :payments, dependent: :destroy
   has_many :reviews, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0, 20]
+      user.name = auth.info.name # assuming the user model has a name
+      user.image = auth.info.image# assuming the user model has an image
+      # If you are using confirmable and the provider(s) you use validate emails,
+      # uncomment the line below to skip the confirmation emails.
+      # user.skip_confirmation!
+    end
+  end
+
+
+
+  # rubocop:disable Lint/AssignmentInCondition
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session['devise.facebook_data'] && session['devise.facebook_data']['extra']['raw_info']
+        user.email = data['email'] if user.email.blank?
+      end
+    end
+  end
 end
