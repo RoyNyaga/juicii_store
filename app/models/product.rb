9db2611f2ff075f9c242efa50754c21fce00e88a
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-12 00:13:01 +0100

    enable deletion of products

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,4 @@
 class Product < ApplicationRecord
-	before_destroy :not_referenced_by_any_line_item
 	belongs_to :category
 	has_many :line_items, dependent: :destroy
 	has_many :reviews, dependent: :destroy
@@ -8,12 +7,4 @@ class Product < ApplicationRecord
 		all.where("lower(title) LIKE :search", search: "%#{search}%")
 	end 
 
-	private
-
-	def not_referenced_by_any_line_item
-		unless line_items.empty?
-			errors.add(:base, "line items present")
-			throw :abort
-		end
-	end
 end
