162cba35169aa0daf3eb1882e2a7623dd6cb3c96
Roy Nyaga <nyagaandreroy@gmail.com>
2020-04-23 01:43:02 +0100

    improved error message for the message model

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,10 +2,10 @@ class MessagesController < ApplicationController
 	def create
 		 @message = Message.create(message_params)
 		 if @message.save
-			 flash[:success] = "Thanks for your message, our team will get back to you in no time"
+			 flash[:success] = "Thanks for your message, our team will get back to you in no time."
 			 redirect_back(fallback_location: root_path)
 		else
-			flash[:danger] = "You message could not be submited please make sure to solve the following issues: #{@message.errors.full_messages.join(",")}"
+			flash[:danger] = "You message could not be submited please make sure to solve the following issues: #{@message.errors.full_messages.join(", ")}"
 			redirect_back(fallback_location: root_path)
 		end
 	end 
