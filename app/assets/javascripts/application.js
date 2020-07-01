f52aa0b256373a5774fa21e847bfc3b667ab0c3e
Roy Nyaga <nyagaandreroy@gmail.com>
2020-07-01 06:01:32 +0100

    add cookie to check for first timers

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,15 +17,29 @@
 
 document.addEventListener('turbolinks:load', function () {
   $(document).ready(() => {
-  	console.log('Yep this works')
+    const addCookie = () => {
+      document.cookie = 'juiciishopFirstTimerCookie=juicii shop; expires=Thu, 18 Dec 2030 12:00:00 UTC'
+      console.log('cookie has been added')
+    }
+
+    const checkCookiePresence = () => {
+      return !!document.cookie.split('; ').includes('juiciishopFirstTimerCookie=juicii shop')
+    }
+
+    function popForm (event) {
+
+    }
+
     const displayPopUpForm = () => {
       var position = $(this).scrollTop()
       console.log(position)
-      if (position > 700) {
+      if (position > 2000 && !checkCookiePresence()) {
+        addCookie()
         $('#modalsignup').modal('show')
       }
     }
 
     window.onscroll = function () { displayPopUpForm() }
+    console.log('Cookie presence:', checkCookiePresence())
   })
 })
